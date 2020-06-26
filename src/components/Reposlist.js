8d37e42146e715a9d9741c610bfa94f3cf5238f5
vaskerraj <yohungdha@gmail.com>
2020-06-26 16:24:01 +0545

    react-moment for last updated date

--- src/components/Reposlist.js
+++ src/components/Reposlist.js
@@ -1,5 +1,6 @@
 import React from 'react';
-
+import Moment from 'react-moment';
+import 'moment-timezone';
 import { faUserAlt, faStar, faEye, faClock} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
@@ -25,7 +26,7 @@ const Reposlist = (props) =>{
                         </div>
                         <div className="card-body">
                             <div className="d-flex">{truncateDescription(description, 50)}</div>
-                            <div className="d-flex mt-2">
+                            <div className="d-flex mt-3">
                                 <span className="d-inline-block pr-2">
                                     <FontAwesomeIcon icon={faUserAlt} className="text-muted mt-1 mr-1"/> {item.owner.login}
                                 </span>
@@ -33,7 +34,7 @@ const Reposlist = (props) =>{
                                     <FontAwesomeIcon icon={faStar} className="text-success" /> {kFormatter(watchers)}
                                 </span>
                             </div>
-                            <div className="d-flex text-black-50 mt-2">
+                            <div className="d-flex text-black-50 text-right mt-1">
                                 <span className="d-inline-block pr-2">
                                     <FontAwesomeIcon icon={faEye} color="text-danger" /> {kFormatter(watchers_count)}
                                 </span>
@@ -41,7 +42,7 @@ const Reposlist = (props) =>{
                                     <FontAwesomeIcon icon={faStar} color="#ddd" /> {kFormatter(forks)}
                                 </span>
                                 <span className="d-inline-block pl-2">
-                                    <FontAwesomeIcon icon={faClock} color="#ddd" /> {updated_at}
+                                    <FontAwesomeIcon icon={faClock} color="#ddd" /> <Moment fromNow>{updated_at}</Moment>
                                 </span>
                             </div>
                         </div>
