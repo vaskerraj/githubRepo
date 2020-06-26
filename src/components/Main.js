19316de211f56c2d304a23bbb8ef38107d819420
vaskerraj <yohungdha@gmail.com>
2020-06-26 06:27:09 +0545

    use hook on form onSubmit and input onChange

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,11 +5,22 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Main = () => {
 
+    const [ searchRepoInput, setSearchRepoInput ] = useState('');
+
+    const handleSearchOnChange = (e) =>{
+        setSearchRepoInput(e.target.value);
+    }
+
+    const handleSearchOnSubmit = (event) =>{
+        event.preventDefault();
+        console.log(`target value : ${searchRepoInput}`);
+
+    }
     return(
         <nav className="navbar navbar-dark bg-dark justify-content-between">
             <div className="navbar-brand">Github</div>
-            <form className="form-inline" >
-            <input className="searchInput" type="search" placeholder="Search" />
+            <form className="form-inline" onSubmit={handleSearchOnSubmit}>
+            <input className="searchInput" type="search" placeholder="Search repository" onChange={handleSearchOnChange} />
             <button className="searchButton my-2 my-sm-0" type="submit">
                 <FontAwesomeIcon icon={faSearch} />
             </button>
