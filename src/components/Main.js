ccb7701460f628868caf84a3b4cc5a2b9a4726e8
vaskerraj <yohungdha@gmail.com>
2020-06-26 06:35:44 +0545

    declare state and add class to prevent blank search

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React , {useState} from 'react';
 import { Link } from 'react-router-dom';
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -6,6 +6,9 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 const Main = () => {
 
     const [ searchRepoInput, setSearchRepoInput ] = useState('');
+    const [isEmptySearchClass, setisEmptySearchClass] = useState('searchInput');
+
+
 
     const handleSearchOnChange = (e) =>{
         setSearchRepoInput(e.target.value);
@@ -15,12 +18,16 @@ const Main = () => {
         event.preventDefault();
         console.log(`target value : ${searchRepoInput}`);
 
+        if(searchRepoInput === ''){
+            setisEmptySearchClass('searchInput empty');
+        }
+
     }
     return(
         <nav className="navbar navbar-dark bg-dark justify-content-between">
             <div className="navbar-brand">Github</div>
             <form className="form-inline" onSubmit={handleSearchOnSubmit}>
-            <input className="searchInput" type="search" placeholder="Search repository" onChange={handleSearchOnChange} />
+            <input className={isEmptySearchClass} type="search" placeholder="Search repository" onChange={handleSearchOnChange} />
             <button className="searchButton my-2 my-sm-0" type="submit">
                 <FontAwesomeIcon icon={faSearch} />
             </button>
