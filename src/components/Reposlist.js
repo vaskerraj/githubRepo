9b15dcee73e605c2770435fca53e7695417005bc
vaskerraj <yohungdha@gmail.com>
2020-06-26 16:37:52 +0545

    fontawesone cdn instead of react-fontawesome

--- src/components/Reposlist.js
+++ src/components/Reposlist.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import Moment from 'react-moment';
 import 'moment-timezone';
-import { faUserAlt, faStar, faEye, faClock} from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Reposlist = (props) =>{
 
@@ -22,7 +20,7 @@ const Reposlist = (props) =>{
                 <div className="card">
                     <div className="card-block">
                         <div className="card-title font-weight-bolder mt-3">
-                            {full_name}
+                            <span className="fa fa-github text-primary"></span> {full_name}
                         </div>
                         <div className="card-body">
                             <div className="d-flex">{truncateDescription(description, 50)}</div>
@@ -30,18 +28,18 @@ const Reposlist = (props) =>{
                         <div className="card-footer font-weight-light mt-2">
                             <div className="row">
                                 <div className="col-8">
-                                    <FontAwesomeIcon icon={faUserAlt} className="text-muted mt-1 mr-1"/> {item.owner.login}
+                                <span className="fa fa-user text-black-50"></span> {item.owner.login}
                                 </div>
                                 <div className="col-4">
-                                    <FontAwesomeIcon icon={faStar} className="text-success" /> {kFormatter(watchers)}
+                                    <span className="fa fa-star-o"></span> {kFormatter(watchers)}
                                 </div>
                             </div>
                             <div className="d-flex mt-1">
                                 <div className="d-inline-block pr-2">
-                                    <FontAwesomeIcon icon={faEye} color="text-danger" /> {kFormatter(watchers_count)}
+                                <span className="fa fa-eye"></span> {kFormatter(watchers_count)}
                                 </div>
-                                <div className="d-inline-block pl-2 pr-4">
-                                    <FontAwesomeIcon icon={faStar} color="#ddd" /> {kFormatter(forks)}
+                                <div className="d-inline-block pl-4 pr-4">
+                                    <span className="fa fa-code-fork"></span> {kFormatter(forks)}
                                 </div>
                                 <div className=" pr-2">
                                     <FontAwesomeIcon icon={faClock} color="#ddd" /> <Moment fromNow>{updated_at}</Moment>
