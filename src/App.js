4f2a36a15e57528d45776b1d4c5955a51b82cf8e
vaskerraj <yohungdha@gmail.com>
2020-06-27 09:49:29 +0545

    add route

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { BrowserRouter, useHistory, Route } from 'react-router-dom';
 import './App.css';
 import Main from './components/Main';
+import RepoDetail from './components/RepoDetail';
 
 function App() {
+
   return (
-    <div className="App">
-      <div className="container-fuild">
-        <Main />
+    <BrowserRouter>
+      <div className="App">
+        <div className="container-fuild">
+            <Route exact={false} path="/" component={ Main } />
+            <Route path="/details/:full_name/:name" component={ RepoDetail } />
+        </div>
       </div>
-    </div>
+    </BrowserRouter>
   );
 }
 
