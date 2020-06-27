c70caae39948b4a0f6d2304950a1278d3fa096a9
vaskerraj <yohungdha@gmail.com>
2020-06-27 13:58:13 +0545

    use github content api for readme

--- src/components/RepoDetail.js
+++ src/components/RepoDetail.js
@@ -5,14 +5,15 @@ const RepoDetail = (props)=>{
     console.log(props);
 
     const [ repoDetail, setRepoDetail ] = useState();
+    const [ repoContent, setRepoContent ] = useState();
 
     const paramsAuthorname = props.match.params.full_name;
     const paramsReponame = props.match.params.name;
 
     useEffect(() => {
         // disable browser overflow
-        document.documentElement.style.overflow = 'hidden';
-        document.body.scroll = "no";
+        // document.documentElement.style.overflow = 'hidden';
+        // document.body.scroll = "no";
 
         receiveReopInfo(paramsAuthorname, paramsReponame);
 
@@ -23,7 +24,12 @@ const RepoDetail = (props)=>{
         // https://developer.github.com/v3/repos/
         const repoInfoUrl = "https://api.github.com/repos/"+author+"/"+name+"";
 
+        // https://developer.github.com/v3/repos/contents/
+        const repoContentUrl = "https://api.github.com/repos/"+author+"/"+name+"/readme";
+
         console.log(repoInfoUrl);
+
+        console.log(repoContentUrl);
 
         await axios.get(repoInfoUrl)
         .then(result => {
@@ -32,7 +38,19 @@ const RepoDetail = (props)=>{
             setRepoDetail(result.data);
 
         });
+
+        await axios.get(repoContentUrl)
+        .then(contentResult => {
+            console.log(contentResult.data);
+
+            var readmeDecode = atob(contentResult.data.content);
+            
+            document.querySelector(".readmeContent").innerHTML = readmeDecode;
+            setRepoContent(readmeDecode);
+
+        });
     }
+
     return(
         <div className="repoDetails">
             <nav className="navbar navbar-dark bg-dark justify-content-between text-white">
@@ -51,11 +69,11 @@ const RepoDetail = (props)=>{
                 { repoDetail &&
                     (
                         <div className="row">
-                            <div className="col-md-4">
+                            <div className="col-md-4 mt-2">
                                 <div className="d-flex">
                                     <img src={repoDetail.owner.avatar_url} className="Owner-avatar" />
                                 </div>
-                                <ul className="list-unstyled">
+                                <ul className="list-unstyled mt-2">
                                     <li><span className="fa fa-user text-muted"></span> { repoDetail.owner.login }</li>
                                     <li className="mt-1"><span className="fa fa-link text-muted"></span> <a href={ repoDetail.owner.html_url } target="_blank">{ repoDetail.owner.html_url }</a></li>
                                     <li className="mt-4"><span className="fa fa-github text-primary"></span> { repoDetail.name }</li>
@@ -66,7 +84,7 @@ const RepoDetail = (props)=>{
                                 
                             </div>
                             <div className="col-md-8 mt-4 border-left border-gray">
-                                
+                                <pre className="readmeContent"></pre>
                             </div>
                         </div>
                     )
